b017ff33ffbb17e3ada570c96f63101d2500de65
bitmole <robert@idea-loop.com>
2024-02-14 09:29:50 -0700

    Parsing error handling

--- src/python_mastery/1_3/pcost.py
+++ src/python_mastery/1_3/pcost.py
@@ -2,16 +2,25 @@
 
 import sys
 
-def total_cost(path):
+def portfolio_cost(path):
     stockdicts = []
     with open(path) as f:
         for row in f:
-            stockdicts.append(
-                    dict(zip(['name', 'shares', 'price'], row.split())))
-    return sum(int(s['shares'])*float(s['price']) for s in stockdicts)
+            name, shares, price = row.split()
+            try:
+                shares = int(shares)
+                price = float(price)
+                d = dict(zip(['name', 'shares', 'price'], (name, shares, price)))
+                print(d)
+                stockdicts.append(d)
+            except ValueError as e:
+                print('Could not parse: ', row, end='')
+                print('Reason: ', e)
+
+    return sum(s['shares'] * s['price'] for s in stockdicts)
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         raise SystemExit('Usage: pcost portfile')
-    print('Total cost:', total_cost(sys.argv[1]))
+    print('Total cost:', portfolio_cost(sys.argv[1]))
